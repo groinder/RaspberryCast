200fb6e86bd5387f149344475c2b4473aa545853
Krzysztof Przybylski <krzysiek7775@gmail.com>
2019-02-11 21:56:28 +0100

    Fix urls

--- process_vlc.py
+++ process_vlc.py
@@ -3,6 +3,7 @@ import os
 import threading
 import logging
 import json
+
 logger = logging.getLogger("RaspberryCast")
 volume = 0
 
@@ -15,99 +16,29 @@ def launchvideo(url, config, sub=False):
     if config["new_log"]:
         os.system("sudo fbi -T 1 -a --noverbose images/processing.jpg")
 
-    logger.info('Extracting source video URL...')
-    out = return_full_url(url, sub=sub, slow_mode=config["slow_mode"])
-
-    logger.debug("Full video URL fetched.")
-
-    thread = threading.Thread(target=play_with_vlc, args=(out, sub,),
+    thread = threading.Thread(target=play_with_vlc, args=(url, sub,),
                               kwargs=dict(width=config["width"], height=config["height"],
-                        new_log=config["new_log"]))
+                                          new_log=config["new_log"]))
     thread.start()
 
     # os.system("echo . > /tmp/cmd &")  # Start signal for OMXplayer
 
 
 def queuevideo(url, config, onlyqueue=False):
-    logger.info('Extracting source video URL, before adding to queue...')
-
-    out = return_full_url(url, sub=False, slow_mode=config["slow_mode"])
-
-    logger.info("Full video URL fetched.")
+    logger.info('Adding to queue...')
 
     if getState() == "0" and not onlyqueue:
         logger.info('No video currently playing, playing video instead of \
 adding to queue.')
-        thread = threading.Thread(target=play_with_vlc, args=(out, False,),
+        thread = threading.Thread(target=play_with_vlc, args=(url, False,),
                                   kwargs=dict(width=config["width"], height=config["height"],
-                        new_log=config["new_log"]))
+                                              new_log=config["new_log"]))
         thread.start()
         # os.system("echo . > /tmp/cmd &")  # Start signal for OMXplayer
     else:
-        if out is not None:
+        if url is not None:
             with open('video.queue', 'a') as f:
-                f.write(out+'\n')
-
-
-def return_full_url(url, sub=False, slow_mode=False):
-    logger.debug("Parsing source url for "+url+" with subs :"+str(sub))
-
-    if ((url[-4:] in (".avi", ".mkv", ".mp4", ".mp3")) or
-            (sub) or (".googlevideo.com/" in url)):
-        logger.debug('Direct video URL, no need to use youtube-dl.')
-        return url
-
-    ydl = youtube_dl.YoutubeDL(
-        {
-            'logger': logger,
-            'noplaylist': True,
-            'ignoreerrors': True,
-        })  # Ignore errors in case of error in long playlists
-    with ydl:  # Downloading youtub-dl infos. We just want to extract the info
-        result = ydl.extract_info(url, download=False)
-
-    if result is None:
-        logger.error(
-            "Result is none, returning none. Cancelling following function.")
-        return None
-
-    if 'entries' in result:  # Can be a playlist or a list of videos
-        video = result['entries'][0]
-    else:
-        video = result  # Just a video
-
-    if "youtu" in url:
-        if slow_mode:
-            for i in video['formats']:
-                if i['format_id'] == "18":
-                    logger.debug(
-                        "Youtube link detected, extracting url in 360p")
-                    return i['url']
-        else:
-            logger.debug('''CASTING: Youtube link detected.
-Extracting url in maximal quality.''')
-            for fid in ('22', '18', '36', '17'):
-                for i in video['formats']:
-                    if i['format_id'] == fid:
-                        logger.debug(
-                            'CASTING: Playing highest video quality ' +
-                            i['format_note'] + '(' + fid + ').'
-                        )
-                        return i['url']
-    elif "vimeo" in url:
-        if slow_mode:
-            for i in video['formats']:
-                if i['format_id'] == "http-360p":
-                    logger.debug("Vimeo link detected, extracting url in 360p")
-                    return i['url']
-        else:
-            logger.debug(
-                'Vimeo link detected, extracting url in maximal quality.')
-            return video['url']
-    else:
-        logger.debug('''Video not from Youtube or Vimeo.
-Extracting url in maximal quality.''')
-        return video['url']
+                f.write(url + '\n')
 
 
 def playlist(url, cast_now, config):
@@ -158,8 +89,8 @@ def play_with_vlc(url, sub, width="", height="", new_log=False):
                     fout.writelines(data[1:])
                 thread = threading.Thread(
                     target=play_with_vlc, args=(first_line, False,),
-                        kwargs=dict(width=width, height=height,
-                                    new_log=new_log),
+                    kwargs=dict(width=width, height=height,
+                                new_log=new_log),
                 )
                 thread.start()
                 # os.system("echo . > /tmp/cmd &")  # Start signal for OMXplayer
@@ -171,7 +102,7 @@ def play_with_vlc(url, sub, width="", height="", new_log=False):
 
 def setState(state):
     # Write to file so it can be accessed from everywhere
-    os.system("echo "+state+" > state.tmp")
+    os.system("echo " + state + " > state.tmp")
 
 
 def getState():
